877b1340c3a79a762a9eabf24aa03cb84557e533
Zach White <zechnwhite@gmail.com>
2020-01-09 22:00:30 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ![form](/img/readmeimg.png)
+
+Make sure to use "npm run dev" as your command, and install all dependencies in both the client and server.
--- server.js
+++ server.js
@@ -18,13 +18,20 @@ app.post('/api/sendmail', (req, res) => {
   const msg = {
     to: 'zechnwhite@gmail.com',
     from: req.body.email,
-    subject: 'whats up',
+    subject: 'A Message for Zach',
     text: req.body.message,
-    html:
-      '<h1 style="color: #5e9ca0;">You can edit <span style="color: #2b2301;">this demo</span> text!</h1> <h2 style="color: #2e6c80;">How to use the editor:</h2> <p>Paste your documents in the visual editor on the left or your HTML code in the source editor in the right. <br />Edit any of the two areas and see the other changing in real time.&nbsp;</p><p>Click the <span style="background-color: #2b2301; color: #fff; display: inline-block; padding: 3px 10px; font-weight: bold; border-radius: 5px;">Clean</span> button to clean your source code.</p>'
+    html: `<h4 style="color: #5e9ca0;">Some moron messaged you and wrote:</h4><p>${req.body.message}</p>`
+    // html: '<h1 style="color: #5e9ca0;">You can edit <span style="color: #2b2301;">this demo</span> text!</h1> <h2 style="color: #2e6c80;">This is a sample sub header for the email:</h2> <p>And here you will find a bunch of random words that mean nothing but at least you can put whatever you want here. Also Peter is a bitch.</p>'
   };
-  sgMail.send(msg);
-  res.json({ message: 'Message received.' });
+  sgMail.send(msg)
+  .then(() => {
+    res.json({ message: 'Message received.' });
+  }
+  )
+  .catch(error => {
+    res.json({ message: 'There was an error sending your email. Zach needs to rewire this so it actually works.' });
+  })
+  
 });
 
 if (process.env.NODE_ENV === 'production') {
